004ebea476d0d42767298209a0d56e3a5994ec74
audreyleow <audreyleow@yahoo.com.sg>
2021-10-26 14:32:50 +0800

    create see more view for each activity type

--- client/src/features/recommendation/FilteredRecommendationView.jsx
+++ client/src/features/recommendation/FilteredRecommendationView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import FilterByCategory from "./FilterByCategory";
 import { Box, Typography } from "@mui/material";
 import { useQuery } from "../../common/hooks/use-query.hook";
