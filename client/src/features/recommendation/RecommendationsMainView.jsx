46f49c52adb71cb35cfb2385e9cf73d7f04c0bc2
audreyleow <audreyleow@yahoo.com.sg>
2021-10-27 00:32:22 +0800

    single activity type route

--- client/src/features/recommendation/RecommendationsMainView.jsx
+++ client/src/features/recommendation/RecommendationsMainView.jsx
@@ -3,6 +3,9 @@ import { Switch, Route, useRouteMatch } from "react-router-dom";
 import ActivityView from "../../common/components/ActivityView";
 import Navbar from "../../common/components/NavBar";
 import FilteredRecommendationView from "./FilteredRecommendationView";
+import SingleActivityTypeView from "./SingleActivityTypeView";
+import { filterOptions } from "../../common/constants/activities-filter-options.contant";
+import _ from "lodash";
 
 export default function RecommendationsMainView() {
   let match = useRouteMatch();
@@ -15,6 +18,13 @@ export default function RecommendationsMainView() {
           2nd <Route> here as an "index" page for all recommendations, or
           the page that is shown when no recommendation is selected */}
       <Switch>
+        {filterOptions.map((filterOption) => {
+          return (
+            <Route path={`${match.path}/${_.kebabCase(filterOption)}`}>
+              <SingleActivityTypeView type={filterOption} />
+            </Route>
+          );
+        })}
         <Route path={`${match.path}/:activityId`}>
           <ActivityView />
         </Route>
