4baa1d47c99d5baa98966365dd5d2eb418892034
audreyleow <audreyleow@yahoo.com.sg>
2021-10-28 21:56:49 +0800

    "updates"

--- client/src/features/recommendation/FilteredRecommendationView.jsx
+++ client/src/features/recommendation/FilteredRecommendationView.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import FilterByCategory from "./FilterByCategory";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Checkbox,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  Menu,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Typography,
+} from "@mui/material";
 import { useQuery } from "../../common/hooks/use-query.hook";
 import axios from "axios";
 import qs from "qs";
 import { filterOptions } from "../../common/constants/activities-filter-options.contant";
 
+const ITEMS_PER_COLUMN = 4;
+
 export default function FilteredRecommendationView() {
   const query = useQuery();
 
-  const [recommendations, setRecommendations] = React.useState();
+  const [recommendations, setRecommendations] = useState();
+
+  const [allChosen, setAllChosen] = React.useState([]);
+
+  const handleAllChosenChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setAllChosen(
+      // On autofill we get a the stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
 
   React.useEffect(() => {
     axios
@@ -42,6 +70,7 @@ export default function FilteredRecommendationView() {
     );
   }
 
+  console.log(allChosen);
   return (
     <Box
       sx={{ display: "flex", flexDirection: "column", padding: "20px 15px" }}
@@ -56,6 +85,34 @@ export default function FilteredRecommendationView() {
       >
         Recommendations
       </Typography>
+      <Box sx={{ display: "flex" }}>
+        <FormControl sx={{ m: 1, width: "70%" }}>
+          <InputLabel id="demo-multiple-checkbox-label">
+            Filter by activity types
+          </InputLabel>
+          <Select
+            multiple
+            value={allChosen}
+            onChange={handleAllChosenChange}
+            input={<OutlinedInput label="Tag" />}
+            renderValue={(selected) => selected.join(", ")}
+          >
+            {filterOptions.map((filterOption) => (
+              <MenuItem key={filterOption} value={filterOption}>
+                <Checkbox
+                  checked={allChosen.indexOf(filterOption) > -1}
+                  name={filterOption}
+                />
+                <ListItemText primary={filterOption} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button variant="outlined" sx={{ m: 1 }}>
+          Filter
+        </Button>
+      </Box>
+
       {Object.entries(recommendations).map(([category, activity]) => (
         <FilterByCategory
           key={category}
